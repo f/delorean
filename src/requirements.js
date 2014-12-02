a702be89756bd253ec4ffd92afec46c709818999
Abhishek Hingnikar <djdark88.7@gmail.com>
2014-12-02 20:28:21 +0530

    Fixed export with

--- src/requirements.js
+++ src/requirements.js
@@ -4,17 +4,42 @@
 // two dependencies now: `EventEmitter` and `Promise`
 var requirements;
 
-module.exports = requirements = {
-  // DeLorean uses **Node.js native EventEmitter** for event emittion
-  EventEmitter: require('events').EventEmitter,
-  // and **es6-promise** for Deferred object management.
-  Promise: require('es6-promise').Promise
-};
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = requirements = {
+    // DeLorean uses **Node.js native EventEmitter** for event emittion
+    EventEmitter: require('events').EventEmitter,
+    // and **es6-promise** for Deferred object management.
+    Promise: require('es6-promise').Promise
+  };
+}
+
+if (typeof define === 'function' && define.amd) {
+  define(function(require, exports, module) {
+    var events = require('events'),
+        promise = require('es6-promise');
+  
+    // Return the module value - http://requirejs.org/docs/api.html#cjsmodule
+    // Using simplified wrapper
+    return { 
+      // DeLorean uses **Node.js native EventEmitter** for event emittion
+      EventEmitter: require('events').EventEmitter,
+      // and **es6-promise** for Deferred object management.
+      Promise: require('es6-promise').Promise
+    };
+  });
+} else {
+  window.DeLorean = DeLorean;
+}
+
+
+
 // It's better you don't change them if you really need to.
+
 
 // This library needs to work for Browserify and also standalone.
 // If DeLorean is defined, it means it's called from the browser, not
 // the browserify.
+
 if (typeof DeLorean !== 'undefined') {
   for (var requirement in requirements) {
     DeLorean.Flux.define(requirement, requirements[requirement]);
